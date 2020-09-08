7f6f9bec60dbb6f9d611440bb3953b7786a290de
cleiton.silva <cleiton.silva@sankhya.com.br>
2020-09-08 19:24:06 -0300

    criando savaUser

--- js/ManegementProcess.js
+++ js/ManegementProcess.js
@@ -1,5 +1,37 @@
-angular.module("ManegementProcess",[]).controller("ManegementProcessCTRL","$scope","$http" [ function($scope, $http){
+angular.module("ManegementProcess", []).controller("ManegementProcessCTRL", function ($scope, $http, $window) {
+
+    $scope.canCreateUser = true;
+    $scope.canCreateAssignment = false;
+
+    $scope.saveUser = saveUser;
+
+    function saveUser(nome, login, password, perfil) {
+
+        const data = {
+            "LOGIN":  login,
+            "NOME":   nome,
+            "PERFIL":  perfil,
+            "PASSWORD": password,
+            "id":  1
+        }
+        
+        daoUser(data)
+    }
+
+    function daoUser(data) {
+        $http.post("http://localhost:8080/user/salvar",  data).then(function (response) {
+            if(response.status == 200){
+                $window.alert("Registro Salvo ComSucesso")
+            }else{
+                $window.alert("Erro: "+ response.status)
+            }
+        });
 
 
+        // $http.post('https://localhost:8080/user/salvar', data)
+        // .then(function (response) {
+        //       $scope.msg = response;
+        //   })
+    }
 
-}])
+});
